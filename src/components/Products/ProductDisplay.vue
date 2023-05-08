2de4d79a59cadd8468d6171c02b7171298a85606
kiwi-ui <yusnianggara@gmail.com>
2023-05-08 11:30:40 +0700

    feat: add conditional index and category

---
+++ src/components/Products/ProductDisplay.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <h1>{{ product.title }}</h1>
+      <img :src="product.image" :alt="product.title" />
+      <p>{{ product.description }}</p>
+    </div>
+    <button @click="fetchProduct">Next</button>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        product: {},
+        index: 1,
+      };
+    },
+    methods: {
+    async fetchProduct() {
+          const response = await fetch(`https://fakestoreapi.com/products/${ this.index <= 20 ? this.index++ : this.index = 1 }`);
+          const data = await response.json();
+          
+          if(data.category == "men's clothing" || data.category == "women's clothing" ) {
+              this.product = data;
+              console.log(data)
+            }
+            else {  }
+    },
+  },
+    mounted() {
+      this.fetchProduct();
+    },
+  };
+  </script>
+  
